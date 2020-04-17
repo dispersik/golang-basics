2ce44f25ea92be1f528c6ad20a9e38388ca846e4
Max Korotkevich <korotkmax@gmail.com>
2020-04-17 17:18:04 +0300

    FIX: now homework4-2 works and prints final result

--- homework4-2.go
+++ homework4-2.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 	"strings"
 	"time"
+	"os"
 )
 
 type Person struct {
@@ -40,20 +41,34 @@ func (p People) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
 
+func (p Person) String() string {
+	return p.firstName+" "+p.lastName+" "+p.birthDay.Format("2006-Jan-02")
+}
+
+func (p People) String() string {
+	result:=""
+	for _, h := range p {
+		result += fmt.Sprintf("%s\n",h)
+	}
+	return result
+}
+
 func main() {
 	ivanIvanovDate, err := time.Parse("2006-Jan-02", "2005-Aug-10")
 	if err != nil {
 		fmt.Println(err)
-		//os.Exit(1)
+		os.Exit(1)
 	}
 	ivanIvanovDate1, err := time.Parse("2006-Jan-02", "2010-Aug-10")
 	if err != nil {
 		fmt.Println(err)
-		//os.Exit(1)
+		os.Exit(1)
 	}
+	
 	p := People{
-		{"Ivan", "Ivanov", ivanIvanovDate}, {"Ivan", "Ivanov", ivanIvanovDate1}}
-	fmt.Println(sort.Sort(p))
-	//fmt.Println(p)
+		{"Ivan", "Ivanov", ivanIvanovDate}, {"Ivan", "Ivanov", ivanIvanovDate1}, {"Alex", "Turner", ivanIvanovDate1}}
+	fmt.Print("In:\n", p)
+	sort.Sort(p)
+	fmt.Print("\nOut:\n", p)
 
 }
